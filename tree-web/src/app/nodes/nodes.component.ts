0d4aa4bae86f32107f64674ae4d77b7631164887
bj <b.janczak90@gmail.com>
2019-03-14 20:52:20 +0100

    create modifying buttons in web app

--- tree-web/src/app/nodes/nodes.component.ts
+++ tree-web/src/app/nodes/nodes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Node } from 'src/app/model/node';
 import { NodeService } from 'src/app/model/node.service';
@@ -12,18 +13,22 @@ export class NodesComponent implements OnInit {
 
   nodeList: Node[];
   dataAvailable: boolean;
+  valueFormDisabled: boolean;
   errorMessage: String;
   selectedTreeNode: Node;
 
   constructor(private nodeService: NodeService) {
     this.selectedTreeNode = null;
     this.dataAvailable = false;
-    this.nodeList = null; }
+    this.nodeList = null;
+    this.valueFormDisabled = false;
+  }
 
   ngOnInit() {
-    this.getNodeList(); }
+    this.getNodeList();
+  }
 
-  getNodeList(): void {
+  public getNodeList(): void {
     this.dataAvailable = true;
 
     this.nodeService.getAllNodes()
@@ -38,8 +43,37 @@ export class NodesComponent implements OnInit {
         error =>  this.errorMessage = <any>error );
   }
 
-  selectNode( node: Node ) : void {
-  this.selectedTreeNode = node;
-  console.log( "Selected node with id:", node.id );
+  public selectNode( node: Node ) : void {
+    this.selectedTreeNode = node;
+    console.log( "Selected node with id:", node.id );
   }
+
+  setNodeValue( valueForm : NgForm) {
+
+  }
+
+  public isNodeSelected() : boolean {
+    return this.selectedTreeNode === null;
+  }
+
+  public disableValueButton() : boolean {
+    return this.isNodeSelected() || this.isLeaf(this.selectedTreeNode);
+  }
+
+  public disableDeleteButton() : boolean {
+    return this.isNodeSelected() || this.isRoot(this.selectedTreeNode);
+  }
+
+  public isLeaf( node : Node ) : boolean {
+    return node.children === null || node.children.length === 0;
+  }
+
+  public isRoot( node : Node ) : boolean {
+    return node === this.nodeList[0];
+  }
+
+  public openForm() : void {
+    this.valueFormDisabled = true;
+  }
+
 }
